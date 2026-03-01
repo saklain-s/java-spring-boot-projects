ba0cd051771d69655b22841bb17f61ae5b74d106
saklain-s <shaiksaklain04@gmail.com>
2026-03-01 22:20:07 +0530

    GlobalExceptions are added

--- profile_managementAPi/src/main/java/com/example/practice/service/impl/ProfileServiceImpl.java
+++ profile_managementAPi/src/main/java/com/example/practice/service/impl/ProfileServiceImpl.java
@@ -3,8 +3,11 @@ package com.example.practice.service.impl;
 import com.example.practice.dto.ProfileCreateRequest;
 import com.example.practice.dto.ProfileResponse;
 import com.example.practice.dto.ProfileUpdateRequest;
+import com.example.practice.entity.Profile;
 import com.example.practice.repository.ProfileRepository;
 import com.example.practice.service.ProfileService;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,26 +20,67 @@ public class ProfileServiceImpl implements ProfileService {
     }
     @Override
     public ProfileResponse createProfile(ProfileCreateRequest request) {
-        return null;
+
+        if (repository.existsByEmail(request.getEmail())){
+            throw new IllegalArgumentException("Email already exists");
+        }
+        Profile profile = new Profile();
+        profile.setName(request.getName());
+        profile.setEmail(request.getEmail());
+        profile.setAge(request.getAge());
+
+        Profile saved = repository.save(profile);
+
+        return mapToResponse(saved);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public ProfileResponse getProfile(Long id) {
-        return null;
+        Profile profile = repository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Profile Not found"));
+        return mapToResponse(profile);
     }
 
     @Override
     public ProfileResponse updateProfile(Long id, ProfileUpdateRequest request) {
-        return null;
+        Profile profile = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Profile Not Found"));
+
+        if (request.getName() != null){
+            profile.setName(request.getName());
+        }
+
+        if (request.getAge() !=null){
+            profile.setAge(request.getAge());
+        }
+        return mapToResponse(profile);
     }
 
     @Override
     public void deleteProfile(Long id) {
 
+        if (!repository.existsById(id)){
+            throw new ResourceNotFoundException("Profile Not Found");
+        }
+        repository.deleteById(id);
     }
 
     @Override
     public List<ProfileResponse> getAllProfiles() {
-        return List.of();
+        return repository.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
+    private ProfileResponse mapToResponse(Profile profile){
+        ProfileResponse response = new ProfileResponse();
+        response.setId(profile.getId());
+        response.setName(profile.getName());
+        response.setEmail(profile.getEmail());
+        response.setAge(profile.getAge());
+
+        return response;
     }
 }
